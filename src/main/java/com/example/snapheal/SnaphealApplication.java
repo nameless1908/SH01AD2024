8e74251aee3b3eea6d15a319804a4242727ffb87
KhanhVu <vuvankhanh022002@gmail.com>
2024-10-04 16:24:35 +0700

    init project

---
+++ src/main/java/com/example/snapheal/SnaphealApplication.java
@@ -0,0 +1,13 @@
+package com.example.snapheal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnaphealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnaphealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/snapheal/SnaphealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snapheal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnaphealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
